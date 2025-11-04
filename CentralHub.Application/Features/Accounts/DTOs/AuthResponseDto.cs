5a1c3927ba7745810baeca1604ea6935f3833983
rvrexp <richuraju77@gmail.com>
2025-11-04 17:54:52 -0600

    Feat: Add registration and login API endpoints (#5)
    Co-authored-by: Richu Raju <richurajuvaz@outlook.com>

---
+++ CentralHub.Application/Features/Accounts/DTOs/AuthResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralHub.Application.Features.Accounts.DTOs
+{
+    public class AuthResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string? Token { get; set; } 
+    }
+}
