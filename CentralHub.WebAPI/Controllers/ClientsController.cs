01664d55427d5a2e5103844382085db117e0588a
Richu Raju <richurajuvaz@outlook.com>
2025-11-06 19:22:34 -0600

    Refactor: Move property endpoints to PropertiesController

--- CentralHub.WebAPI/Controllers/ClientsController.cs
+++ CentralHub.WebAPI/Controllers/ClientsController.cs
@@ -1,9 +1,6 @@
-﻿using CentralHub.Application.Features.Clients.Commands.AddPropertyToClient;
-using CentralHub.Application.Features.Clients.Commands.CreateClient;
+﻿using CentralHub.Application.Features.Clients.Commands.CreateClient;
 using CentralHub.Application.Features.Clients.Commands.DeleteClient;
-using CentralHub.Application.Features.Clients.Commands.DeleteProperty;
 using CentralHub.Application.Features.Clients.Commands.UpdateClient;
-using CentralHub.Application.Features.Clients.Commands.UpdateProperty;
 using CentralHub.Application.Features.Clients.Queries.GetAllClients;
 using CentralHub.Application.Features.Clients.Queries.GetClientById;
 using CentralHub.Application.Interfaces;
@@ -98,46 +95,6 @@ namespace CentralHub.WebAPI.Controllers
             await _mediator.Send(command);
 
             // Return a 204 No Content, which is the standard for a successful DELETE
-            return NoContent();
-        }
-        // POST /api/clients/{clientId}/properties
-        [HttpPost("{clientId:guid}/properties")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> AddPropertyToClient(Guid clientId, [FromBody] AddPropertyToClientCommand command)
-        {
-            command.ClientId = clientId;
-            var propertyId = await _mediator.Send(command);
-
-            // We can build a "GetPropertyById" endpoint later. For now, just return the ID.
-            return CreatedAtAction(nameof(GetClientById), new { id = clientId }, new { newPropertyId = propertyId });
-        }
-
-        // PUT /api/clients/{clientId}/properties/{propertyId}
-        [HttpPut("{clientId:guid}/properties/{propertyId:guid}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateProperty(Guid clientId, Guid propertyId, [FromBody] UpdatePropertyCommand command)
-        {
-            // Set the IDs from the route
-            command.ClientId = clientId;
-            command.PropertyId = propertyId;
-
-            await _mediator.Send(command);
-
-            return NoContent(); // Standard for a successful PUT
-        }
-
-        // DELETE /api/clients/{clientId}/properties/{propertyId}
-        [HttpDelete("{clientId:guid}/properties/{propertyId:guid}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteProperty(Guid clientId, Guid propertyId)
-        {
-            var command = new DeletePropertyCommand { ClientId = clientId, PropertyId = propertyId };
-            await _mediator.Send(command);
             return NoContent();
         }
     }
